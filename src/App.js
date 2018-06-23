8b7b42eeb619046c93d499fe845aec94eed47fc5
maxnawa31 <mnawa31@gmail.com>
2018-06-22 21:55:03 -0700

    starting mobile portfolio

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Homepage from './components/Homepage'
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <Homepage/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Homepage.css'
+const Homepage = () => {
+  return (
+    <div className='homepage-container'>
+      <div className='text-box'>
+      My name is <span className="name-text">Maximillian Nawa</span> and I am a creatively driven software engineer based in<span className="skills-text">San Francisco, California.</span>
+
+      </div>
+    </div>
+  )
+}
+
+export default Homepage;
